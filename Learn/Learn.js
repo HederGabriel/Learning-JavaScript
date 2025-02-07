a7d7baf46ed2f1827dbdebea941b4f25a85a8b21
Heder <hedergabrielsalessousa@gmail.com>
2025-02-07 10:00:56 -0300

    Nacionalidade

--- Learn/Learn.js
+++ Learn/Learn.js
@@ -1,6 +1,7 @@
-var vel = 40;
-console.log(`A velocidade do seu carro é ${vel}Km/h`);
-if (vel > 60) {
-  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+var país = 'Brasil'
+console.log(`Nasceu no ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else{
+    console.log('Você é Estrangeiro')
 }
-console.log("Dirija sempre usando cinto de segurança!");
