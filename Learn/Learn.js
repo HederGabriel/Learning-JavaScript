8b5df3d0c73f187a477bf8f897b1e05bc1fa2542
Heder <hedergabrielsalessousa@gmail.com>
2025-02-07 14:04:40 -0300

    Switch(Condição Múltipla)

--- Learn/Learn.js
+++ Learn/Learn.js
@@ -1,10 +1,37 @@
 var agora = new Date()
-var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
-if (hora < 12) {
-    console.log('Bom dia!')
-} else if(hora <=18) {
-    console.log('Boa tarde!')
-} else{
-    console.log('Boa noite!')
+var diaSem = agora.getDay()
+/*
+0 = Domingo
+1 = Segunda-feira
+2 = Terça-feira
+3 = Quarta-feira
+4 = Quinta-feira
+5 = Sexta-feira
+6 = Sábado
+*/
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2: 
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Dia inválido')
+        break
 }
