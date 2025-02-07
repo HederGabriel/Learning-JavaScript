505deedbf0126ca5192d9aaf88dca78742e9bffa
Heder <hedergabrielsalessousa@gmail.com>
2025-02-07 10:13:09 -0300

    Site DETRAN

--- Learn/Learn.js
+++ Learn/Learn.js
@@ -1,7 +1,10 @@
-var país = 'Brasil'
-console.log(`Nasceu no ${país}`)
-if (país == 'Brasil') {
-    console.log('Você é Brasileiro')
-} else{
-    console.log('Você é Estrangeiro')
+function calcular(){
+    var txtv = document.getElementById("txtvel");
+    var res = document.getElementById("res");
+    var vel = Number(txtv.value);
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}Km/h</strong></p>`;
+    if(vel > 60){
+        res.innerHTML += `<p>Você foi multado por excesso de veloocidade</p>`
+    }
+    res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`;
 }
