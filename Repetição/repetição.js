153e94537a05da3c4c05d7e909a5a770e056d50f
Heder <hedergabrielsalessousa@gmail.com>
2025-02-07 21:07:32 -0300

    Introdução Repetição

---
+++ Repetição/repetição.js
@@ -0,0 +1,16 @@
+/* While(Test no Inicio)
+var c = 1
+while(c <= 1){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/* do(Test no final)
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6)
+*/
+
