dc1fb2846c641637ec360ab8379a343755d984a5
Heder <hedergabrielsalessousa@gmail.com>
2025-02-08 17:54:48 -0300

    Learn Array/Vetor

---
+++ Arrays/array.js
@@ -0,0 +1,36 @@
+/* Cria um vetor
+let num = [2, 1, 3, 3]
+*/
+
+/* Adiciona um elemento no final do vetor
+num.push(4) 
+*/
+
+/* Retorna o tamanho do vetor
+num.length  
+*/
+
+/* Ordena o vetor por ordem crescente
+num.sort() 
+*/
+
+/* Retorna elemento de posição específica
+console.log(`O vetor da posição 0 é ${num[0]}`)
+*/
+
+/* Mostrar os elementos de Maneira Customizada
+for(let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem valor ${num[pos]}`)
+}
+*/
+
+/* Maneira otimizada do 👆
+for(let pos in num){
+    console.log(`A posição ${pos} tem valor ${num[pos]}`)
+}
+*/
+
+/* Buscar a posição de um elemento
+var busca = num.indexOf(4)
+console.log(`${busca}`)
+*/
